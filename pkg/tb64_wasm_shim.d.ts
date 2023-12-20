1f5d774273f21f2614d32545199acd517e606fc8
pictographer <corbett@pictographer.com>
2023-12-20 14:09:55 -0800

    Trying with web-pack build --target web.

--- pkg/tb64_wasm_shim.d.ts
+++ pkg/tb64_wasm_shim.d.ts
@@ -86,3 +86,46 @@ export class JsTaggedBase64 {
 export class TaggedBase64 {
   free(): void;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly parse_tb64: (a: number, b: number, c: number) => void;
+  readonly encode_tb64: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbg_taggedbase64_free: (a: number) => void;
+  readonly __wbg_jstaggedbase64_free: (a: number) => void;
+  readonly to_string: (a: number, b: number) => void;
+  readonly jstaggedbase64_new: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly jstaggedbase64_parse: (a: number, b: number, c: number) => void;
+  readonly jstaggedbase64_tag: (a: number, b: number) => void;
+  readonly jstaggedbase64_value: (a: number, b: number) => void;
+  readonly jstaggedbase64_set_tag: (a: number, b: number, c: number) => void;
+  readonly jstaggedbase64_set_value: (a: number, b: number, c: number) => void;
+  readonly jstaggedbase64_to_string: (a: number, b: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
