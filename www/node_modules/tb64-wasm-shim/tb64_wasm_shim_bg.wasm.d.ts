a7ad1fc184cec399bd66bfb6b2eceee70fc53019
pictographer <corbett@pictographer.com>
2023-12-22 11:55:36 -0800

    Maybe Git Hub Pages doesn't like symbolic links?

---
+++ www/node_modules/tb64-wasm-shim/tb64_wasm_shim_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function parse_tb64(a: number, b: number, c: number): void;
+export function encode_tb64(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbg_taggedbase64_free(a: number): void;
+export function __wbg_jstaggedbase64_free(a: number): void;
+export function to_string(a: number, b: number): void;
+export function jstaggedbase64_new(a: number, b: number, c: number, d: number, e: number): void;
+export function jstaggedbase64_parse(a: number, b: number, c: number): void;
+export function jstaggedbase64_tag(a: number, b: number): void;
+export function jstaggedbase64_value(a: number, b: number): void;
+export function jstaggedbase64_set_tag(a: number, b: number, c: number): void;
+export function jstaggedbase64_set_value(a: number, b: number, c: number): void;
+export function jstaggedbase64_to_string(a: number, b: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
